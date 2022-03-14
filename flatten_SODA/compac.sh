204a30dc51b8da6be63e5ed33e465c09f1252e3a
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2022-03-14 15:16:07 -0300

    update

---
+++ flatten_SODA/compac.sh
@@ -0,0 +1,5 @@
+zip -r /media/thiago/b69516cd-ab99-487f-9f04-062f0eaea877/train_divisions_1.zip train_divisions/ -d ./
+
+zip -r /media/thiago/b69516cd-ab99-487f-9f04-062f0eaea877/test_divisions_1.zip test_divisions/ -d ./
+
+
